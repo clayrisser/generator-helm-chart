66b80d4b713dfc4a1d950f6241ee816ca3ca19cb
Jam Risser <jam@codejam.ninja>
2018-10-13 12:16:04 -0500

    Added more resources

--- src/writing.js
+++ src/writing.js
@@ -31,10 +31,30 @@ export default async function writing(yo) {
     yo.destinationPath('values.yaml'),
     yo.context
   );
+  yo.fs.copyTpl(
+    yo.templatePath('template/shared/values.yaml'),
+    yo.destinationPath('values.yaml'),
+    yo.context
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('template/shared/templates/NOTES.txt'),
+    yo.destinationPath('templates/NOTES.txt'),
+    yo.context
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('template/shared/templates/_helpers.tpl'),
+    yo.destinationPath('templates/_helpers.tpl'),
+    yo.context
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('template/shared/templates/pvc.yaml'),
+    yo.destinationPath('templates/pvc.yaml'),
+    yo.context
+  );
   if (_.find(yo.context.deployments, deployment => deployment.public)) {
     yo.fs.copyTpl(
       yo.templatePath('template/shared/templates/certificate.yaml'),
-      yo.destinationPath('template/certificate.yaml'),
+      yo.destinationPath('templates/certificate.yaml'),
       yo.context
     );
   }
@@ -42,7 +62,7 @@ export default async function writing(yo) {
   if (configMaps.length) {
     yo.fs.copyTpl(
       yo.templatePath('template/shared/templates/configmap.yaml'),
-      yo.destinationPath('template/configmap.yaml'),
+      yo.destinationPath('templates/configmap.yaml'),
       {
         ...yo.context,
         configMaps
@@ -53,45 +73,45 @@ export default async function writing(yo) {
   if (configSecrets.length) {
     yo.fs.copyTpl(
       yo.templatePath('template/shared/templates/secret.yaml'),
-      yo.destinationPath('template/secret.yaml'),
+      yo.destinationPath('templates/secret.yaml'),
       {
         ...yo.context,
         configSecrets
       }
     );
   }
-  // yo.context.deployments.forEach(deployment => {
-  //   const context = {
-  //     ...yo.context,
-  //     deployment
-  //   };
-  //   if (deployment.public) {
-  //     yo.fs.copyTpl(
-  //       yo.templatePath('template/shared/template/deployments/public.yaml'),
-  //       yo.destinationPath(`template/deployments/${deployment.name}.yaml`),
-  //       context
-  //     );
-  //     yo.fs.copyTpl(
-  //       yo.templatePath('template/shared/template/services/public.yaml'),
-  //       yo.destinationPath(`template/services/${deployment.name}.yaml`),
-  //       context
-  //     );
-  //     yo.fs.copyTpl(
-  //       yo.templatePath('template/shared/template/ingress.yaml'),
-  //       yo.destinationPath(`template/ingresses/${deployment.name}.yaml`),
-  //       context
-  //     );
-  //   } else {
-  //     yo.fs.copyTpl(
-  //       yo.templatePath('template/shared/template/deployments/private.yaml'),
-  //       yo.destinationPath(`template/deployments/${deployment.name}.yaml`),
-  //       yo.context
-  //     );
-  //     yo.fs.copyTpl(
-  //       yo.templatePath('template/shared/template/services/private.yaml'),
-  //       yo.destinationPath(`template/services/${deployment.name}.yaml`),
-  //       context
-  //     );
-  //   }
-  // });
+  yo.context.deployments.forEach(deployment => {
+    const context = {
+      ...yo.context,
+      deployment
+    };
+    if (deployment.public) {
+      yo.fs.copyTpl(
+        yo.templatePath('template/shared/templates/deployments/public.yaml'),
+        yo.destinationPath(`templates/deployments/${deployment.name}.yaml`),
+        context
+      );
+      yo.fs.copyTpl(
+        yo.templatePath('template/shared/templates/services/public.yaml'),
+        yo.destinationPath(`templates/services/${deployment.name}.yaml`),
+        context
+      );
+      yo.fs.copyTpl(
+        yo.templatePath('template/shared/templates/ingress.yaml'),
+        yo.destinationPath(`templates/ingresses/${deployment.name}.yaml`),
+        context
+      );
+    } else {
+      yo.fs.copyTpl(
+        yo.templatePath('template/shared/templates/deployments/private.yaml'),
+        yo.destinationPath(`templates/deployments/${deployment.name}.yaml`),
+        yo.context
+      );
+      yo.fs.copyTpl(
+        yo.templatePath('template/shared/templates/services/private.yaml'),
+        yo.destinationPath(`templates/services/${deployment.name}.yaml`),
+        context
+      );
+    }
+  });
 }
