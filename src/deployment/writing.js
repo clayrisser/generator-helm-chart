6524aa8339380811fd424ed11140f3543869e15d
Jam Risser <jam@codejam.ninja>
2018-10-14 00:46:42 -0500

    Added inline modifying questions.yaml

--- src/deployment/writing.js
+++ src/deployment/writing.js
@@ -1,20 +1,20 @@
+import modInline from 'mod-inline';
 import path from 'path';
-import _ from 'lodash';
 
 export default async function writing(yo) {
-  // yo.fs.copyTpl(
-  //   yo.templatePath('questions.yaml'),
-  //   yo.destinationPath('questions.yaml'),
-  //   yo.context
-  // );
   yo.fs.copy(
-    path.resolve(process.cwd(), 'values.yaml'),
+    path.resolve(yo.context.destination, 'questions.yaml'),
+    yo.destinationPath('questions.yaml'),
+    { process: content => processQuestions(content, yo.context) }
+  );
+  yo.fs.copy(
+    path.resolve(yo.context.destination, 'values.yaml'),
     yo.destinationPath('values.yaml'),
     { process: content => processValues(content, yo.context) }
   );
   if (yo.context.deployment.public) {
     yo.fs.copy(
-      path.resolve(process.cwd(), 'templates/certificate.yaml'),
+      path.resolve(yo.context.destination, 'templates/certificate.yaml'),
       yo.destinationPath('templates/certificate.yaml'),
       { process: content => processCertificate(content, yo.context) }
     );
@@ -63,7 +63,7 @@ function processCertificate(content, context) {
   const COMMON_NAME = / {2}commonName:[^\n]+/;
   const DNS_NAMES = / {2}dnsNames:(\n {4}- [^\n]+)+/;
   if (DNS_NAMES.test(content)) {
-    content = inline.append(
+    content = modInline.append(
       content,
       DNS_NAMES,
       `\n    - '{{ (index .Values.ingress.hosts.${
@@ -71,7 +71,7 @@ function processCertificate(content, context) {
       } 0).name }}'`
     );
   } else {
-    content = inline.append(
+    content = modInline.append(
       content,
       COMMON_NAME,
       `\n  dnsNames:\n    - '{{ (index .Values.ingress.hosts.${
@@ -79,7 +79,7 @@ function processCertificate(content, context) {
       } 0).name }}'`
     );
   }
-  content = inline.append(
+  content = modInline.append(
     content,
     DOMAINS,
     `\n        - '{{ (index .Values.ingress.hosts.${
@@ -89,11 +89,49 @@ function processCertificate(content, context) {
   return content;
 }
 
+function processQuestions(content, context) {
+  content = content.toString();
+  if (context.deployment.public) {
+    const INGRESS_ENABLED = / {2}- variable: ingress\.enabled((\n {4}[^\n]+)+)?/;
+    const SERVICE_TYPE = / {2}- variable: service\.type((\n {4}[^\n]+)+)?/;
+    const SUBQUESTIONS = / {4}subquestions:((\n {6}[^\n]+)+)?/;
+    content = modInline.append(
+      content,
+      [INGRESS_ENABLED, SUBQUESTIONS],
+      `\n      - variable: ingress.hosts.${
+        context.deployment.name
+      }[0].name\n        default: xip.io\n        description: 'hostname to your ${
+        context.deployment.name
+      } installation'\n        type: hostname\n        required: true\n        label: '${
+        context.deployment.name
+      } hostname'\n      - variable: ingress.hosts.${
+        context.deployment.name
+      }[0].path\n        default: /\n        description: 'pathname to your ${
+        context.deployment.name
+      } installation'\n        type: string\n        required: true\n        label: '${
+        context.deployment.name
+      } path'`
+    );
+    content = modInline.append(
+      content,
+      [SERVICE_TYPE, SUBQUESTIONS],
+      `\n      - variable: service.nodePorts.${
+        context.deployment.name
+      }.http\n        default: ''\n        description: 'NodePort ${
+        context.deployment.name
+      } http port (to set explicitly, choose port between 30000-32767)'\n        type: int\n        min: 30000\n        max: 32767\n        show_if: ingress.enabled=false&&service.type=NodePort\n        label: '${
+        context.deployment.name
+      } http port'`
+    );
+  }
+  return content;
+}
+
 function processValues(content, context) {
   content = content.toString();
   const IMAGES = /images:((\n {2}[^\n]+)+)?/;
   const { image } = context.deployment;
-  content = inline.append(
+  content = modInline.append(
     content,
     IMAGES,
     `\n  ${context.deployment.name}:\n    repository: ${image.substr(
@@ -108,12 +146,12 @@ function processValues(content, context) {
     const INGRESS = /ingress:((\n {2}[^\n]+)+)/;
     const NODE_PORTS = / {2}nodePorts:((\n {4}[^\n]+)+)?/;
     const SERVICE = /service:((\n {2}[^\n]+)+)/;
-    content = inline.append(
+    content = modInline.append(
       content,
       [SERVICE, NODE_PORTS],
       `\n    ${context.deployment.name}:\n      http: ''`
     );
-    content = inline.append(
+    content = modInline.append(
       content,
       [INGRESS, HOSTS],
       `\n    ${context.deployment.name}:\n      - name: ''\n        path: /`
@@ -121,33 +159,3 @@ function processValues(content, context) {
   }
   return content;
 }
-
-const inline = {
-  append(content, regexes, appendValue) {
-    if (!_.isArray(regexes)) regexes = [regexes];
-    if (regexes.length > 1) {
-      const regex = regexes[0];
-      const isolatedContent = inline.isolate(content, regex);
-      content = content.replace(
-        isolatedContent,
-        inline.append(isolatedContent, [...regexes].pop(), appendValue)
-      );
-    } else {
-      const regex = regexes[0];
-      const matches = content.match(regex);
-      if (matches && matches.length) {
-        const match = matches[0];
-        content = content.replace(match, `${match}${appendValue}`);
-      }
-    }
-    return content;
-  },
-  isolate(content, regex) {
-    const matches = content.match(regex);
-    if (matches && matches.length) {
-      const match = matches[0];
-      return match;
-    }
-    return null;
-  }
-};
