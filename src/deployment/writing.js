b10b09566b2fa75d6160a0836ab906c228b8e677
Jam Risser <jam@codejam.ninja>
2018-10-13 21:52:05 -0500

    Added dynamic certificate modification

--- src/deployment/writing.js
+++ src/deployment/writing.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default async function writing(yo) {
   // yo.fs.copyTpl(
   //   yo.templatePath('questions.yaml'),
@@ -10,11 +12,11 @@ export default async function writing(yo) {
   //   yo.context
   // );
   if (yo.context.deployment.public) {
-    // yo.fs.copyTpl(
-    //   yo.templatePath('templates/certificate.yaml'),
-    //   yo.destinationPath('templates/certificate.yaml'),
-    //   yo.context
-    // );
+    yo.fs.copy(
+      path.resolve(process.cwd(), 'templates/certificate.yaml'),
+      yo.destinationPath('templates/certificate.yaml'),
+      { process: content => processCertificate(content, yo.context) }
+    );
     yo.fs.copyTpl(
       yo.templatePath('templates/deployments/public.yaml'),
       yo.destinationPath(
@@ -52,4 +54,43 @@ export default async function writing(yo) {
       yo.context
     );
   }
+}
+
+function processCertificate(content, context) {
+  content = content.toString();
+  const DOMAINS = / {6}domains:(\n {8}- [^\n]+)+/;
+  const COMMON_NAME = / {2}commonName:[^\n]+/;
+  const DNS_NAMES = / {2}dnsNames:(\n {4}- [^\n]+)+/;
+  let matches = content.match(DNS_NAMES);
+  if (matches && matches.length) {
+    const match = matches[0];
+    content = content.replace(
+      match,
+      `${match}
+    - '{{ (index .Values.ingress.hosts.${context.deployment.name} 0).name }}'`
+    );
+  } else {
+    matches = content.match(COMMON_NAME);
+    if (matches && matches.length) {
+      const match = matches[0];
+      content = content.replace(
+        match,
+        `${match}
+  dnsNames:
+    - '{{ (index .Values.ingress.hosts.${context.deployment.name} 0).name }}'`
+      );
+    }
+  }
+  matches = content.match(DOMAINS);
+  if (matches && matches.length) {
+    const match = matches[0];
+    content = content.replace(
+      match,
+      `${match}
+        - '{{ (index .Values.ingress.hosts.${
+          context.deployment.name
+        } 0).name }}'`
+    );
+  }
+  return content;
 }
