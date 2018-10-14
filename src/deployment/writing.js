cfab9d35ff5911674e4e67f31d03523251e5b8dc
Jam Risser <jam@codejam.ninja>
2018-10-13 23:50:48 -0500

    Dynamically update values

--- src/deployment/writing.js
+++ src/deployment/writing.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import _ from 'lodash';
 
 export default async function writing(yo) {
   // yo.fs.copyTpl(
@@ -6,11 +7,11 @@ export default async function writing(yo) {
   //   yo.destinationPath('questions.yaml'),
   //   yo.context
   // );
-  // yo.fs.copyTpl(
-  //   yo.templatePath('values.yaml'),
-  //   yo.destinationPath('values.yaml'),
-  //   yo.context
-  // );
+  yo.fs.copy(
+    path.resolve(process.cwd(), 'values.yaml'),
+    yo.destinationPath('values.yaml'),
+    { process: content => processValues(content, yo.context) }
+  );
   if (yo.context.deployment.public) {
     yo.fs.copy(
       path.resolve(process.cwd(), 'templates/certificate.yaml'),
@@ -61,36 +62,92 @@ function processCertificate(content, context) {
   const DOMAINS = / {6}domains:(\n {8}- [^\n]+)+/;
   const COMMON_NAME = / {2}commonName:[^\n]+/;
   const DNS_NAMES = / {2}dnsNames:(\n {4}- [^\n]+)+/;
-  let matches = content.match(DNS_NAMES);
-  if (matches && matches.length) {
-    const match = matches[0];
-    content = content.replace(
-      match,
-      `${match}
-    - '{{ (index .Values.ingress.hosts.${context.deployment.name} 0).name }}'`
+  if (DNS_NAMES.test(content)) {
+    content = inline.append(
+      content,
+      DNS_NAMES,
+      `\n    - '{{ (index .Values.ingress.hosts.${
+        context.deployment.name
+      } 0).name }}'`
     );
   } else {
-    matches = content.match(COMMON_NAME);
-    if (matches && matches.length) {
-      const match = matches[0];
-      content = content.replace(
-        match,
-        `${match}
-  dnsNames:
-    - '{{ (index .Values.ingress.hosts.${context.deployment.name} 0).name }}'`
-      );
-    }
+    content = inline.append(
+      content,
+      COMMON_NAME,
+      `\n  dnsNames:\n    - '{{ (index .Values.ingress.hosts.${
+        context.deployment.name
+      } 0).name }}'`
+    );
   }
-  matches = content.match(DOMAINS);
-  if (matches && matches.length) {
-    const match = matches[0];
-    content = content.replace(
-      match,
-      `${match}
-        - '{{ (index .Values.ingress.hosts.${
-          context.deployment.name
-        } 0).name }}'`
+  content = inline.append(
+    content,
+    DOMAINS,
+    `\n        - '{{ (index .Values.ingress.hosts.${
+      context.deployment.name
+    } 0).name }}'`
+  );
+  return content;
+}
+
+function processValues(content, context) {
+  content = content.toString();
+  const IMAGES = /images:((\n {2}[^\n]+)+)?/;
+  const { image } = context.deployment;
+  content = inline.append(
+    content,
+    IMAGES,
+    `\n  ${context.deployment.name}:\n    repository: ${image.substr(
+      0,
+      image.indexOf(':')
+    )}\n    tag: ${image.substr(
+      image.indexOf(':') + 1
+    )}\n    pullPolicy: IfNotPresent`
+  );
+  if (context.deployment.public) {
+    const HOSTS = / {2}hosts:((\n {4}[^\n]+)+)?/;
+    const INGRESS = /ingress:((\n {2}[^\n]+)+)/;
+    const NODE_PORTS = / {2}nodePorts:((\n {4}[^\n]+)+)?/;
+    const SERVICE = /service:((\n {2}[^\n]+)+)/;
+    content = inline.append(
+      content,
+      [SERVICE, NODE_PORTS],
+      `\n    ${context.deployment.name}:\n      http: ''`
+    );
+    content = inline.append(
+      content,
+      [INGRESS, HOSTS],
+      `\n    ${context.deployment.name}:\n      - name: ''\n        path: /`
     );
   }
   return content;
 }
+
+const inline = {
+  append(content, regexes, appendValue) {
+    if (!_.isArray(regexes)) regexes = [regexes];
+    if (regexes.length > 1) {
+      const regex = regexes[0];
+      const isolatedContent = inline.isolate(content, regex);
+      content = content.replace(
+        isolatedContent,
+        inline.append(isolatedContent, [...regexes].pop(), appendValue)
+      );
+    } else {
+      const regex = regexes[0];
+      const matches = content.match(regex);
+      if (matches && matches.length) {
+        const match = matches[0];
+        content = content.replace(match, `${match}${appendValue}`);
+      }
+    }
+    return content;
+  },
+  isolate(content, regex) {
+    const matches = content.match(regex);
+    if (matches && matches.length) {
+      const match = matches[0];
+      return match;
+    }
+    return null;
+  }
+};
