065f999a7a29d026d899dee7d2689e9b37ce7db0
Jam Risser <jam@codejam.ninja>
2018-10-14 00:05:59 -0500

    Automatically guess name for deployment

--- src/deployment/prompting.js
+++ src/deployment/prompting.js
@@ -1,12 +1,21 @@
 import YoBasePrompts from 'yo-base-prompts';
+import _ from 'lodash';
+import fs from 'fs-extra';
+import yaml from 'js-yaml';
+import path from 'path';
 
 export default async function prompting(yo) {
   const yoBasePrompts = new YoBasePrompts(yo);
-  yoBasePrompts.name = name;
-  const { destination, name } = await yoBasePrompts.prompt({
-    destination: true,
-    name: true
+  if (!_.includes(process.argv, '--destination')) {
+    process.argv.push(`--destination=${process.cwd()}`);
+  }
+  yoBasePrompts.name = 'placeholder';
+  const { destination } = await yoBasePrompts.prompt({
+    destination: true
   });
+  const { name } = yaml.safeLoad(
+    fs.readFileSync(path.resolve(destination, 'Chart.yaml'), 'utf8')
+  );
   let deployment = await yo.prompt([
     {
       message: 'Deployment Name:',
